7f3163b60c282c01f892400a7ff56d3930b587aa
mdjewel999 <mdjewel878761@gmail.com>
2023-04-09 01:54:47 +0600

    cart jsx code added

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 
-const Cart = () => {
+const Cart = ({cart, handleRemoveFromCart}) => {
+    let message;
+    if(cart.length === 0){
+        message = <p>Please add some products </p>
+    }
+    else{
+        message = <div>
+            <h3>vai onak Borolx</h3>
+            <p><small>Thanks for giving your money</small></p>
+        </div>
+    }
     return (
         <div>
-            <h1>order summary </h1>
+            <h1>order summary: {cart.length} </h1>
+            {cart.length > 2 ? <span>Aro kino</span> : <span>Fokira</span>}
+            {message}
+            {
+                cart.map(tshirt => <p 
+                    key={tshirt._id}
+                    >{tshirt.name} <button
+                     onClick={() =>handleRemoveFromCart(tshirt._id)}
+                     >Remove</button></p>)
+            }
+            {
+                cart.length === 2 && <p>Doble Boranja</p>
+            }
+            {
+                cart.length === 3 || <h3>Tinta to hoilo na !! </h3>
+            }
         </div>
     );
 };
 
 export default Cart;
+/* 
+*   conditional readering
+1. use if else to set a variable that will contain an element , components
+2. ternary operator : condition ? 'for true' : 'false'
+3. Logical &&: (if the condition is true then the next thing will be displayed)
+4. Logical || (if the condition is false then the next thing will be displayed)
+*/
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,14 +2,28 @@ import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import Tshirt from "../Tshirt/Tshirt";
 import Cart from "../Cart/Cart";
-import './Home.css'
+import './Home.css';
+import{ Toaster, toast } from 'react-hot-toast';
 
 const Home = () => {
   const tshirts = useLoaderData();
   const [cart, setCart] = useState([]);
 
 const handleAddToCart = tshirt =>{
-        console.log(tshirt)
+  const exists = cart.find(ts => ts._id === tshirt._id)
+  if(exists){
+ toast('You have already added this t-shirt')
+  }
+  else{
+    const newCart = [...cart, tshirt];
+    setCart(newCart)
+
+  }
+}
+
+const handleRemoveFromCart = id =>{
+  const remainig = cart.filter(ts => ts._id !== id);
+  setCart(remainig);
 }
 
   return (
@@ -24,7 +38,10 @@ const handleAddToCart = tshirt =>{
         ))}
       </div>
       <div className="cart-container">
-            <Cart></Cart>
+            <Cart
+             cart={cart}
+             handleRemoveFromCart ={handleRemoveFromCart}
+             ></Cart>
       </div>
     </div>
 
